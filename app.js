15812c40e88ac19b957424cbda641710cb7b6db0
AlanoxSwe <61416243+AlanoxSwe@users.noreply.github.com>
2020-11-27 10:18:12 +0100

    sandbox testing finish

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Populartimes = require('@christophern/populartimesjs').Populartimes;
+const GooglePlaceProvider = require('google-place-provider');
 require('dotenv').config();
 
 const { GOOGLE_API_KEY } = process.env; 
-const populartimes = new Populartimes();
+const populartimes = new Populartimes(GOOGLE_API_KEY);
 
 const app = express();
 app.use(bodyParser());
 
 // ChIJx2wugWCdX0YRESbzi9wBIr4 - T-Centralen
 
-app.get('/api/densities', (req, res) => {
-  populartimes.fullWeek('ChIJy-SYzgy1YEYRc6DEL7x_slY').then((data) => {
-    res.json(data);
-  });
+async function main() {
+  try {
+    const googlePlace = new GooglePlaceProvider(GOOGLE_API_KEY);
+
+    const textSearch = await googlePlace.textSearch({
+        query: 't-centralen',
+    });
+    console.log(textSearch);
+  } catch (e) {
+    throw e;
+  }
+}
+
+app.get('/api/densities', async (req, res) => {
+  await main();
+  // populartimes.placeDetails('ChIJx2wugWCdX0YRESbzi9wBIr4').then((data) => {
+  //   res.json(data);
+  // });
+  res.end();
 });
 
 
