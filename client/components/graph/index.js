9fe62ecf327c216c5658c9ff814cde0bab57e489
AlanoxSwe <61416243+AlanoxSwe@users.noreply.github.com>
2020-12-02 13:53:04 +0100

    fixed indentation

--- client/components/graph/index.js
+++ client/components/graph/index.js
@@ -5,59 +5,59 @@ import ThemeContext from "../../theme/Context";
 
 
 export default function LineGraph({ data }) {
- const { invertedThemeClass, themeClass, spinnerThemeColor, graphThemeColor } = useContext(ThemeContext);
+  const { invertedThemeClass, themeClass, spinnerThemeColor, graphThemeColor } = useContext(ThemeContext);
 
- const labels = [...Array(24).keys()].map((i) => i.toString() + ':00');
+  const labels = [...Array(24).keys()].map((i) => i.toString() + ':00');
 
- const graphData = {
-   labels: labels,
-   datasets: [
-     {
-       fill: false,
-       lineTension: 0.5,
-       pointBackgroundColor: graphThemeColor,
-       borderColor: graphThemeColor,
-       pointRadius: "0",
-       borderWidth: "3",
-       data,
-     },
-   ],
- };
+  const graphData = {
+    labels: labels,
+    datasets: [
+      {
+        fill: false,
+        lineTension: 0.5,
+        pointBackgroundColor: graphThemeColor,
+        borderColor: graphThemeColor,
+        pointRadius: "0",
+        borderWidth: "3",
+        data,
+      },
+    ],
+  };
 
- const options = {
-   legend: { display: false },
-   responsive: true,
-   maintainAspectRatio: false,
-   scales: {
-     xAxes: [
-       {
-         gridLines: {
-           display: false,
-         },
-         ticks: {
-           beginAtZero: true,
-           autoSkip: true,
-           maxTicksLimit: 11,
-         },
-       },
-     ],
-     yAxes: [
-       {
-         ticks: {
-           beginAtZero: true,
-           autoSkip: true,
-           maxTicksLimit: 10,
-         },
-       },
-     ],
-   },
- };
+  const options = {
+    legend: { display: false },
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      xAxes: [
+        {
+          gridLines: {
+            display: false,
+          },
+          ticks: {
+            beginAtZero: true,
+            autoSkip: true,
+            maxTicksLimit: 11,
+          },
+        },
+      ],
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+            autoSkip: true,
+            maxTicksLimit: 10,
+          },
+        },
+      ],
+    },
+  };
 
- merge(defaults, {
-   global: {
-     defaultFontColor: spinnerThemeColor,
-   },
- });
+  merge(defaults, {
+    global: {
+      defaultFontColor: spinnerThemeColor,
+    },
+  });
 
     return (
         <div className={`data-wrapper`}>
