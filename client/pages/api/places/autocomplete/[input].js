5e6ae12772b32d572707606190306df8e00458b4
mar-veloper <42553243+mar-veloper@users.noreply.github.com>
2020-12-03 10:53:00 +0100

    fixed location on development mode

--- client/pages/api/places/autocomplete/[input].js
+++ client/pages/api/places/autocomplete/[input].js
@@ -1,13 +1,25 @@
 import GooglePlaceProvider from "google-place-provider";
+import Axios from "axios";
 const { GOOGLE_API_KEY } = process.env;
 const googlePlace = new GooglePlaceProvider(GOOGLE_API_KEY);
 
 export default async (req, res) => {
   const { input, location } = req.query;
+  const {
+    data: { location: geolocation },
+  } = await Axios.post(
+    `https://www.googleapis.com/geolocation/v1/geolocate?key=${GOOGLE_API_KEY}`
+  );
+
+  const devLoc = `${geolocation.lat},${geolocation.lng}`;
+
+  const userLocation =
+    process.env.NODE_ENV === "development" ? devLoc : location;
+
   try {
     const autocomplete = await googlePlace.autocomplete({
       input,
-      location,
+      location: devLoc,
       radius: 300,
     });
     return res.status(200).json(autocomplete);
