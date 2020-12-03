60fe0cad7cff8965a9791ea5875ef02882b393da
mar-veloper <42553243+mar-veloper@users.noreply.github.com>
2020-12-03 14:35:20 +0100

    fixed autocomplete

--- pages/api/places/autocomplete/[input].js
+++ pages/api/places/autocomplete/[input].js
@@ -5,21 +5,10 @@ const googlePlace = new GooglePlaceProvider(GOOGLE_API_KEY);
 
 export default async (req, res) => {
   const { input, location } = req.query;
-  const {
-    data: { location: geolocation },
-  } = await Axios.post(
-    `https://www.googleapis.com/geolocation/v1/geolocate?key=${GOOGLE_API_KEY}`
-  );
-
-  const devLoc = `${geolocation.lat},${geolocation.lng}`;
-
-  const userLocation =
-    process.env.NODE_ENV === "development" ? devLoc : location;
-
   try {
     const autocomplete = await googlePlace.autocomplete({
       input,
-      location: userLocation,
+      location,
       radius: 300,
     });
     return res.status(200).json(autocomplete);
